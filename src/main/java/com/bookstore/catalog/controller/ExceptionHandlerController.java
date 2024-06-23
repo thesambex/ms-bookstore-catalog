7ca1d4c92cb587426a29c208dc99406b0b6db4cc
Samuel Ives <sivesssl@gmail.com>
2024-06-23 20:07:59 -0300

    errors improvement

--- src/main/java/com/bookstore/catalog/controller/ExceptionHandlerController.java
+++ src/main/java/com/bookstore/catalog/controller/ExceptionHandlerController.java
@@ -7,12 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.Date;
+import java.util.*;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
@@ -20,7 +21,6 @@ public class ExceptionHandlerController {
     private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlerController.class);
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleGeneralError(Exception e, WebRequest request) {
         logger.error("Error: {}", e.getMessage(), e);
         var error = new ErrorResponse(e.getMessage(), request.getDescription(false), null, new Date());
@@ -29,7 +29,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e, WebRequest request) {
-        var error = new ErrorResponse(e.getMessage(), request.getDescription(false), null, new Date());
+        var error = new ErrorResponse(e.getMessage(), request.getDescription(false), e.getExtra(), new Date());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
@@ -37,6 +37,21 @@ public class ExceptionHandlerController {
     public ResponseEntity<ErrorResponse> handleConflictException(ConflictException e, WebRequest request) {
         var error = new ErrorResponse(e.getMessage(), request.getDescription(false), e.getExtra(), new Date());
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldError = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldError, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    private record InvalidParams(String cause, String attr) {
     }
 
 }
--- src/main/java/com/bookstore/catalog/domain/exceptions/NotFoundException.java
+++ src/main/java/com/bookstore/catalog/domain/exceptions/NotFoundException.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
+    private String extra;
+
     public NotFoundException(String message) {
         super(message);
+    }
+
+    public NotFoundException(String message, String extra) {
+        super(message);
+        this.extra = extra;
+    }
+
+    public String getExtra() {
+        return extra;
     }
 
 }
